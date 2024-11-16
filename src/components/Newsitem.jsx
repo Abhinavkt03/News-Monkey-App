9c9d0f262ebb11906258f131ec7097543f2e72a2
Abhinav Tayal <140099230+Abhinavkt03@users.noreply.github.com>
2024-11-16 16:42:06 +0530

    Add files via upload

---
+++ src/components/Newsitem.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Newsitem({ title, description, imgUrl, url }) {
+  return (
+    <div className="card" style={{ width: "22rem", height: "100%" }}>
+      <img
+        src={imgUrl}
+        className="card-img-top"
+        alt="..."
+        style={{ objectFit: "fill", height: "20rem" }}
+      />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+        <a href={url} rel="noreferrer" target="_blank" className="btn btn-dark">
+          Read more
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Newsitem;
